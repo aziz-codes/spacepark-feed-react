bef566f4fccd3e672239043b2b421ad9feca6233
aziz-codes <azizisoffline@gmail.com>
2023-02-11 16:19:55 +0500

    tailwind config, code refactor,basic setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/*.{js,jsx}", "./src/components/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
