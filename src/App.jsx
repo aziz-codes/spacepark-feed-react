3588cacaf4a27b9ce3ce84f06aa70616bf960c53
aziz-codes <azizisoffline@gmail.com>
2023-02-11 16:28:44 +0500

    main Layout,heroicons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
+import {
+  Navbar,
+  Sidebar,
+  Stories,
+  Feed,
+  RightSidebar,
+} from "./components/index";
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <section>
+      <Navbar />
+    </section>
+  );
 };
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from "./Navbar";
+export { default as Feed } from "./Feed";
+export { default as RightSidebar } from "./RightSidebar";
+export { default as Sidebar } from "./Sidebar";
+export { default as Stories } from "./Stories";
