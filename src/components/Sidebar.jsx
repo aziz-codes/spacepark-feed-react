4dcdcd395ae3cc76ef2f78bf431c0fedac454179
aziz-codes <azizisoffline@gmail.com>
2023-02-11 17:45:28 +0500

    sticky navbar  and fixed sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div className=" w-56 bg-sky-500">Sidebar</div>;
+  return (
+    <div className="sm:w-20 md:w-56 bg-sky-500 h-screen">
+      <div className="fixed w-full flex flex-col gap-3"></div>
+    </div>
+  );
 };
 
 export default Sidebar;
