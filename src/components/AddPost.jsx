4340dd3fe0c6d88bec1586eb3c4743964bfc6651
aziz-codes <azizisoffline@gmail.com>
2023-02-17 00:01:46 +0500

    add post picture

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -3,10 +3,11 @@ import user from "../assets/user.png";
 import { ShareIcon } from "@heroicons/react/24/outline";
 import Button from "../layouts/Button";
 const AddPost = () => {
+  let user = JSON.parse(localStorage.getItem("user"));
   return (
     <section className="pl-4 pr-4 w-full bg-white shadow-md rounded-sm pt-4 pb-4 flex flex-row justify-between max-w-lg">
       <div className="flex flex-row gap-3 flex-1">
-        <img src={user} className="h-8 w-8 object-cover " />
+        <img src={user && user.photoURL} className="h-8 w-8 object-cover " />
         <input
           type="tex"
           placeholder="What's new, alendra ?"
