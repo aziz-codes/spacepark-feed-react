e622e01d24279c5bdec6fa453c0aee9cde0c9962
aziz-codes <azizisoffline@gmail.com>
2023-02-17 16:14:21 +0500

    API ID

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   projectId: "spacepark-f6c46",
   storageBucket: "spacepark-f6c46.appspot.com",
   messagingSenderId: "657707527178",
-  appId: "1:657707527178:web:5f8dd943bdc24d9e2543f3",
+  appId: import.meta.env.VITE_API_ID,
 };
 
 const app = initializeApp(firebaseConfig);
