51395f1a171af419d6777d46ca9f62a4d64303d7
aziz-codes <azizisoffline@gmail.com>
2023-02-17 16:15:05 +0500

    final touch with env

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: import.meta.env.VITE_API_KEY, //rename this with your own api key from firebase
   authDomain: "spacepark-f6c46.firebaseapp.com",
   projectId: "spacepark-f6c46",
   storageBucket: "spacepark-f6c46.appspot.com",
   messagingSenderId: "657707527178",
-  appId: import.meta.env.VITE_API_ID,
+  appId: import.meta.env.VITE_API_ID, //replace this with your own app id.
 };
 
 const app = initializeApp(firebaseConfig);
